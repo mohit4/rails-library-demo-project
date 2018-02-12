f8a2631c2dba54d65e8a29532c64c4eb0ec77f28
mohit4 <mohitkumar2801@gmail.com>
2018-02-12 14:32:45 -0500

    create new template, action and route

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   get 'welcome/index'
   root 'welcome#home'
   get 'about', to: 'welcome#about'
+  
+  resources :articles
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
