bc9cede8474debccd905403844915ed934e972ca
mohit4 <mohitkumar2801@gmail.com>
2018-02-17 09:04:06 -0500

    Fix created, created show and handled error messages

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -4,10 +4,18 @@ class ArticlesController < ApplicationController
   end
   
   def create
-    #render plain: params[:article].inspect
     @article = Article.new(article_params)
-    @article.save
-    redirect_to articles_show(@article)
+    if @article.save  # article passes the validations
+      flash[:notice] = "Article was successfully created!"
+      redirect_to article_path(@article)
+    else
+      # do something else
+      render 'new' # go to create new article
+    end
+  end
+  
+  def show
+    @article = Article.find(params[:id])
   end
   
   private
