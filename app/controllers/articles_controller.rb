f942766f8e05b97c8d623b99a3fb751956dccde5
mohit4 <mohitkumar2801@gmail.com>
2018-02-24 08:00:47 -0500

    Added user and article associations and debugger

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -11,7 +11,9 @@ class ArticlesController < ApplicationController
   end
   
   def create
+    #debugger
     @article = Article.new(article_params)
+    @article.user = User.first
     if @article.save  # article passes the validations
       flash[:success] = "Article was successfully created!"
       redirect_to article_path(@article)
