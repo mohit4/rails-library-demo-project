93cd5b6fb499974cbd84a3e8047d6542724d84f5
mohit_kirar <mohitkumar2801@gmail.com>
2018-04-11 16:26:02 -0400

    updated current_user for new article

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -15,7 +15,7 @@ class ArticlesController < ApplicationController
   def create
     #debugger
     @article = Article.new(article_params)
-    @article.user = User.first
+    @article.user = current_user
     if @article.save  # article passes the validations
       flash[:success] = "Article was successfully created!"
       redirect_to article_path(@article)
